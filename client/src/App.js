4334befd080d5b53d8c2d8c16b77d6e36880613c
Mitchell Moore <mmoorejr24@gmail.com>
2019-12-02 17:30:47 -0800

    setup mockjobs to display them on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 
+import Jobs from './Jobs';
+
+const mockJobs = [
+	{title: 'SWE 1', company: 'Google'},
+	{title: 'SWE 1', company: 'Facebook'},
+	{title: 'SWE 1', company: 'Apple'}
+]
+
 function App() {
   return (
-    <div className="App">
-      <div>
-        <h1>Hello There</h1>
-      </div>
-    </div>
+	<div className="App">
+	    <Jobs jobs={mockJobs} />
+	</div>
   );
 }
 
